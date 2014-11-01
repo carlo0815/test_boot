328ea7a7894a7704305bc45714304d13fcd670ca
toto <toto_golden@gmx.de>
2014-11-01 13:56:13 +0100

    Update nfr4xbm.cpp
    add Driverversion XpeedLX1+2

--- nfr4xbm.cpp
+++ nfr4xbm.cpp
@@ -93,6 +93,17 @@ nfr4xbm::nfr4xbm()
 					temps.replace(pos, 1, "");
 				sprintf(driver, "[ %s ]", temps.c_str());
 			}
+			if (strstr(buf, "Package: ini-dvb-modules-inihde")) {
+				fgets(buf, 256, f);
+				temps = string(buf);
+				pos = temps.find(' ');
+				if(pos != string::npos)
+					 temps = temps.substr(pos);
+				pos = temps.find('\n');
+				if(pos != string::npos)
+					temps.replace(pos, 1, "");
+				sprintf(driver, "[ %s ]", temps.c_str());
+			}
 			if (strstr(buf, "Package: dreambox-secondstage")) {
 				fgets(buf, 256, f);
 				temps = string(buf);
