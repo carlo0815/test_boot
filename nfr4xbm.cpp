51211211f7385efe084e95401614f0ea4f130cef
toto <toto_golden@gmx.de>
2014-11-01 19:56:11 +0100

    Update nfr4xbm.cpp

--- nfr4xbm.cpp
+++ nfr4xbm.cpp
@@ -137,7 +137,18 @@ nfr4xbm::nfr4xbm()
 					temps.replace(pos, 1, "");
 				sprintf(driver, "[ %s ]", temps.c_str());
 			}
-			if (strstr(buf, "Package: xp-dvb-modules-xp1000 ")) {
+			if (strstr(buf, "Package: xp-dvb-modules-xp1000")) {
+				fgets(buf, 256, f);
+				temps = string(buf);
+				pos = temps.find(' ');
+				if(pos != string::npos)
+					 temps = temps.substr(pos);
+				pos = temps.find('\n');
+				if(pos != string::npos)
+					temps.replace(pos, 1, "");
+				sprintf(driver, "[ %s ]", temps.c_str());
+			}
+			if (strstr(buf, "Package: odin-dvb-modules-odinm7s")) {
 				fgets(buf, 256, f);
 				temps = string(buf);
 				pos = temps.find(' ');
